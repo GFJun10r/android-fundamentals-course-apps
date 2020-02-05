988533dfe7864654fd66be4a68d04e217b1b598d
Gerson Ferreira Junior <geferreira@uolinc.com>
2020-02-05 14:48:56 -0300

    Adding CountUp button

--- DiceRoller/app/src/main/java/com/gfjun10r/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/gfjun10r/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gfjun10r.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +13,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+        val countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener{countUp()}
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val inicialText = getString(R.string.initial_result_text)
+
+        val textValue = resultText.text.toString()
+
+        if (textValue.equals(inicialText)){
+            resultText.text = 1.toString()
+        }else{
+            var diceValue = textValue.toInt()
+            if (diceValue < 6){
+                resultText.text = (diceValue + 1).toString()
+            }else{
+                Toast.makeText(this, "Dice reached max value...", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun rollDice() {
-        val randomInt = (1..6).random()
         val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = (1..6).random()
         resultText.text = randomInt.toString()
     }
 }
