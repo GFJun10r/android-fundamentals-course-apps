2a87913c97ff4a1833626165d751ff0d244147d8
Gerson Ferreira Junior <geferreira@uolinc.com>
2020-02-05 14:16:32 -0300

    Adding Click handler for roll Button and changing inicial result_text

--- DiceRoller/app/src/main/java/com/gfjun10r/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/gfjun10r/diceroller/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.gfjun10r.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
     }
 
-    private fun rollDice(){
-        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+    private fun rollDice() {
+        val randomInt = (1..6).random()
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
     }
 }
