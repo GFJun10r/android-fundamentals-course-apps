7875639ae1e10542b16d82eb229d621b55eac6f7
Gerson Ferreira Junior <geferreira@uolinc.com>
2020-02-05 18:48:02 -0300

    Adding Dice images and removing Count up Button

--- DiceRoller/app/src/main/java/com/gfjun10r/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/gfjun10r/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.gfjun10r.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -16,40 +17,27 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
-        val countUpButton: Button = findViewById(R.id.count_up_button)
-        countUpButton.setOnClickListener { countUp() }
-
         val resetButton: Button = findViewById(R.id.reset_button)
         resetButton.setOnClickListener { resetResult() }
 
     }
 
-    private fun resetResult() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = getString(R.string.initial_result_text)
-    }
-
-    private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        val inicialText = getString(R.string.initial_result_text)
-
-        if (resultText.text.toString() == inicialText) {
-            resultText.text = "1"
-        } else {
-            var diceValue = resultText.text.toString().toInt()
-
-            if (diceValue < 6) {
-                diceValue++
-                resultText.text = diceValue.toString()
-            } else {
-                Toast.makeText(this, "Dice reached max value...", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = (1..6).random()
-        resultText.text = randomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
+    }
+
+    private fun resetResult() {
+
     }
 }
